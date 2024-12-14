28cc31b91b17263eac9fe34caead81da05e63a59
Kotaro Abe <abekoh.bcky@gmail.com>
2024-12-14 17:08:35 +0900

    fix api

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"log"
-	"os"
-	"strings"
-
 	ddlambda "github.com/DataDog/datadog-lambda-go"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go-v2/config"
 	_ "github.com/aws/aws-sdk-go-v2/service/accessanalyzer"
 	_ "github.com/aws/aws-sdk-go-v2/service/account"
 	_ "github.com/aws/aws-sdk-go-v2/service/acm"
@@ -338,7 +331,7 @@ import (
 	_ "github.com/aws/aws-sdk-go-v2/service/route53recoveryreadiness"
 	_ "github.com/aws/aws-sdk-go-v2/service/route53resolver"
 	_ "github.com/aws/aws-sdk-go-v2/service/rum"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
+	_ "github.com/aws/aws-sdk-go-v2/service/s3"
 	_ "github.com/aws/aws-sdk-go-v2/service/s3control"
 	_ "github.com/aws/aws-sdk-go-v2/service/s3outposts"
 	_ "github.com/aws/aws-sdk-go-v2/service/s3tables"
@@ -416,66 +409,11 @@ import (
 	_ "github.com/aws/aws-sdk-go-v2/service/xray"
 )
 
-type Order struct {
-	OrderID string  `json:"order_id"`
-	Amount  float64 `json:"amount"`
-	Item    string  `json:"item"`
-}
-
-var (
-	s3Client *s3.Client
-)
-
-func init() {
-	// Initialize the S3 client outside of the handler, during the init phase
-	cfg, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		log.Fatalf("unable to load SDK config, %v", err)
-	}
-
-	s3Client = s3.NewFromConfig(cfg)
-}
-
-func uploadReceiptToS3(ctx context.Context, bucketName, key, receiptContent string) error {
-	_, err := s3Client.PutObject(ctx, &s3.PutObjectInput{
-		Bucket: &bucketName,
-		Key:    &key,
-		Body:   strings.NewReader(receiptContent),
-	})
-	if err != nil {
-		log.Printf("Failed to upload receipt to S3: %v", err)
-		return err
-	}
-	return nil
-}
-
-func handleRequest(ctx context.Context, event json.RawMessage) error {
-	// Parse the input event
-	var order Order
-	if err := json.Unmarshal(event, &order); err != nil {
-		log.Printf("Failed to unmarshal event: %v", err)
-		return err
-	}
-
-	// Access environment variables
-	bucketName := os.Getenv("RECEIPT_BUCKET")
-	if bucketName == "" {
-		log.Printf("RECEIPT_BUCKET environment variable is not set")
-		return fmt.Errorf("missing required environment variable RECEIPT_BUCKET")
-	}
-
-	// Create the receipt content and key destination
-	receiptContent := fmt.Sprintf("OrderID: %s\nAmount: $%.2f\nItem: %s",
-		order.OrderID, order.Amount, order.Item)
-	key := "receipts/" + order.OrderID + ".txt"
-
-	// Upload the receipt to S3 using the helper method
-	if err := uploadReceiptToS3(ctx, bucketName, key, receiptContent); err != nil {
-		return err
-	}
-
-	log.Printf("Successfully processed order %s and stored receipt in S3 bucket %s", order.OrderID, bucketName)
-	return nil
+func handleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       request.Body,
+	}, nil
 }
 
 func main() {
