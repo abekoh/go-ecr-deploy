84b1271de6c18c55e5f6255413c03efcfa57b4dd
Kotaro Abe <abekoh.bcky@gmail.com>
2024-12-14 17:18:27 +0900

    add lambda

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,6 +61,68 @@ resource "aws_iam_access_key" "ecr_user_access_key" {
   user = aws_iam_user.ecr_user.name
 }
 
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Effect = "Allow"
+        Sid    = ""
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name = "go-ecr-deploy-example"
+  role          = aws_iam_role.lambda_exec_role.arn
+  package_type  = "Image"
+  image_uri    = "${aws_ecr_repository.repository.repository_url}:multistage-copy-nocache"
+}
+
+resource "aws_api_gateway_rest_api" "http_request_api" {
+  name        = "http-request-api"
+}
+
+resource "aws_api_gateway_resource" "root" {
+  rest_api_id = aws_api_gateway_rest_api.http_request_api.id
+  parent_id   = aws_api_gateway_rest_api.http_request_api.root_resource_id
+  path_part   = "request"
+}
+
+resource "aws_api_gateway_method" "get_request" {
+  rest_api_id   = aws_api_gateway_rest_api.http_request_api.id
+  resource_id   = aws_api_gateway_resource.root.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.http_request_api.id
+  resource_id             = aws_api_gateway_resource.root.id
+  http_method             = aws_api_gateway_method.get_request.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "http_request_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.http_request_api.id
+}
+
+resource "aws_lambda_permission" "allow_apigateway" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  statement_id  = "AllowAPIGatewayInvoke"
+}
+
 output "AWS_ACCESS_KEY_ID" {
   value = aws_iam_access_key.ecr_user_access_key.id
 }
