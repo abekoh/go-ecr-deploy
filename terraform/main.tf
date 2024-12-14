7bb677f8ab9b763d5a4b9993b11aca8ed578d8f3
Kotaro Abe <abekoh.bcky@gmail.com>
2024-12-14 17:42:39 +0900

    fix terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -114,6 +114,13 @@ resource "aws_api_gateway_integration" "lambda_integration" {
 
 resource "aws_api_gateway_deployment" "http_request_deployment" {
   rest_api_id = aws_api_gateway_rest_api.http_request_api.id
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.http_request_api.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_lambda_permission" "allow_apigateway" {
