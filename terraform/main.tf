8cc64d589ecc4ff1525dcccb45e354b31846031c
Kotaro Abe <abekoh.bcky@gmail.com>
2024-12-14 15:32:05 +0900

    update tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,43 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_ecr_repository" "foo" {
+resource "aws_ecr_repository" "repository" {
   name                 = "go-ecr-deploy"
   image_tag_mutability = "MUTABLE"
+}
+
+resource "aws_iam_user" "ecr_user" {
+  name = "go-ecr-deploy"
+}
+
+resource "aws_iam_policy" "ecr_publish_policy" {
+  name        = "go-ecr-deploy-publish-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "ecr_user_policy" {
+  user       = aws_iam_user.ecr_user.name
+  policy_arn = aws_iam_policy.ecr_publish_policy.arn
+}
+
+resource "aws_iam_access_key" "ecr_user_access_key" {
+  user = aws_iam_user.ecr_user.name
+}
+
+output "AWS_ACCESS_KEY_ID" {
+  value = aws_iam_access_key.ecr_user_access_key.id
 }
